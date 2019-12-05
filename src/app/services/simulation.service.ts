74e1f18b074140180a77df6b8b1659e21863e50b
Your Name <you@example.com>
2019-12-05 11:42:14 +0100

    Import du module Http dans le service simulation

--- src/app/services/simulation.service.ts
+++ src/app/services/simulation.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
@@ -7,8 +7,16 @@ import { HttpClient } from "@angular/common/http";
 export class SimulationService {
   constructor(private httpClient: HttpClient) {}
 
-  getRate(id) {
-    return this.httpClient
-  }
+  url = "https://omgvamp-hear0thstone-v1.p.rapidapi.com/";
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json"
+    })
+  };
+
+  // Méthode pour envoyer les informations de la simulation et récupérer le taux d'intérêt calculé en java
+  postInformationSimulation(id) {
+    // return this.httpClient.post();
+  }
 }
