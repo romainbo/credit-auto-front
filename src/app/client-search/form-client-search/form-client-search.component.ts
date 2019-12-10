d311f654c68c578f6afe459b0ab722388ab4af37
Your Name <you@example.com>
2019-12-10 15:42:26 +0100

    Version du front qui fonctionne avec java pour creation et simulation

--- src/app/client-search/form-client-search/form-client-search.component.ts
+++ src/app/client-search/form-client-search/form-client-search.component.ts
@@ -1,45 +1,35 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
-import {ClientSearchService} from "../../services/client-search.service";
-import {BehaviorSubject, merge, Observable} from "rxjs";
-import {switchMap} from "rxjs/operators";
-import {Client} from "../../client-creation/client";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import { ClientSearchService } from "../../services/client-search.service";
+import { BehaviorSubject, merge, Observable } from "rxjs";
+import { switchMap } from "rxjs/operators";
+import { Client } from "../../client-creation/client";
 
 @Component({
-  selector: 'app-form-client-search',
-  templateUrl: './form-client-search.component.html',
-  styleUrls: ['./form-client-search.component.scss']
+  selector: "app-form-client-search",
+  templateUrl: "./form-client-search.component.html",
+  styleUrls: ["./form-client-search.component.scss"]
 })
 export class FormClientSearchComponent implements OnInit {
-
   email: string;
   isSearched = false;
-  foundClient : Client;
+  foundClient: Client;
 
+  searchForm = new FormGroup({
+    email: new FormControl("")
+  });
 
-  searchForm = new FormGroup(
-    {
-      email: new FormControl("")
-    }
-  )
+  constructor(private clientSearchService: ClientSearchService) {}
 
-  constructor(private clientSearchService: ClientSearchService) {
-  }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   submit() {
     this.email = this.searchForm.get("email").value;
     this.isSearched = true;
 
-    this.clientSearchService
-      .getClientByEmail(this.email)
-      .subscribe(client =>
-      this.foundClient = client);
-
-
-
-    console.log(this.foundClient);
+    this.clientSearchService.getClientByEmail(this.email).subscribe(client => {
+      this.foundClient = client;
+      console.log(this.foundClient);
+    });
   }
 }
